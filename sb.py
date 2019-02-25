de85d9bb238c2f13cc1937291b9dda2bfa9e860c
Flushuine <aqillatas33@gmail.com>
2019-02-25 17:56:20 +0700

    Update sb.py

--- sb.py
+++ sb.py
@@ -26,12 +26,7 @@ while True:
                 if msg.text != None:
                     if msg.toType == 2:
                         may = client.getProfile().mid
-                        if may in str(msg.contentMetadata) and 'MENTION' in str(msg.contentMetadata):
-                            pilih = ['yang tag sy semoga jomblo seumur hidup','ngapain tag tag woe, kangen?','ada apa ini? ko di tag?','duhh kena tag, dianya kesepian kali yah','gk usah tag, gift tikel aja']
-                            rslt = random.choice(pilih)
-                            client.sendText(msg.to, str(rslt))
-                        else:
-                            pass
+
                     else:
                         pass
                 else:
@@ -151,7 +146,7 @@ while True:
                                 pass
                             else:
                                 cctv['sidermem'][op.param1] += "\n~ " + Name
-                                pref=['eh ada','hai kak','aloo..','nah','lg ngapain','halo','sini kak']
+                                pref=['Sider Spotted','Another Sider']
                                 client.sendText(op.param1, str(random.choice(pref))+' '+Name)
                         else:
                             pass
