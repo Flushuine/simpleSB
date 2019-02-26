3566f55b89a8019c9e60cc6da611bd87cb6fac77
Flushuine <aqillatas33@gmail.com>
2019-02-26 19:51:16 +0700

    add confirmation feature

--- sb.py
+++ sb.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from linepy import *
+from random import *
 import json, time, random, sys
 
 client = LineClient()
@@ -21,170 +22,175 @@ while True:
     try:
         ops=poll.singleTrace(count=50)
         for op in ops:
-            if op.type == 26:
-                msg = op.message
-                if msg.text != None:
-                    if msg.toType == 2:
-                        msg = op.message
-                        text = msg.text
-                        msg_id = msg.id
-                        receiver = msg.to
-                        sender = msg._from
-                        may = client.getProfile().mid
-                        if msg.text.lower() == "hai gainbot":
-                            client.sendText(receiver, "Ashiyyapp!")
-                        elif msg.text.lower() == "scan":
-                            client.sendText(receiver, "Done!")
-                            try:
-                                del cctv['point'][msg.to]
-                                del cctv['sidermem'][msg.to]
-                                del cctv['cyduk'][msg.to]
-                            except:
-                                pass
-                            cctv['point'][receiver] = msg.id
-                            cctv['sidermem'][receiver] = ""
-                            cctv['cyduk'][receiver]=True
-                        elif msg.text.lower() == "sider":
-                                if msg.to in cctv['point']:
-                                    cctv['cyduk'][msg.to]=False
-                                    client.sendText(receiver, "Sider Spotted:\n" + cctv['sidermem'][msg.to])
-                                else:
-                                    client.sendText(msg.to, "Please scan your read point first")
-                        else:
-                            pass
+            msg = op.message
+            text = msg.text
+            msg_id = msg.id
+            receiver = msg.to
+            sender = msg._from
+            cek = 0
+            if cek == 0:
+                num = randint(0,4)
+                client.sendText(receiver, num)
+                pref=['bAmBANG', 'MaLiH', 'kUntal kUntUl', 'BamBANG', 'MAlih']
+                client.sendText(receiver, "Please type '" + pref[num] + "' before continue")
+                if msg.text == pref[num]:
+                    cek = 1
                 else:
                     pass
-            elif op.type == 25:
-                msg = op.message
-                text = msg.text
-                msg_id = msg.id
-                receiver = msg.to
-                sender = msg._from
-                try:
-                    if msg.contentType == 0:
+            else:
+                if op.type == 26:
+                    if msg.text != None:
                         if msg.toType == 2:
-                            client.sendChatChecked(receiver, msg_id)
-                            contact = client.getContact(sender)
-                            if text.lower() == 'me':
-                                client.sendMessage(receiver, None, contentMetadata={'mid': sender}, contentType=13)
-                            elif text.lower() == 'speed':
-                                start = time.time()
-                                client.sendText(receiver, "TestSpeed")
-                                elapsed_time = time.time() - start
-                                client.sendText(receiver, "%s detik" % (elapsed_time))
-                            elif 'spic' in text.lower():
-                                try:
-                                    key = eval(msg.contentMetadata["MENTION"])
-                                    u = key["MENTIONEES"][0]["M"]
-                                    a = client.getContact(u).pictureStatus
-                                    client.sendImageWithURL(receiver, 'http://dl.profile.line.naver.jp/'+a)
-                                except Exception as e:
-                                    client.sendText(receiver, str(e))
-                            elif 'scover' in text.lower():
-                                try:
-                                    key = eval(msg.contentMetadata["MENTION"])
-                                    u = key["MENTIONEES"][0]["M"]
-                                    a = channel.getProfileCoverURL(mid=u)
-                                    client.sendImageWithURL(receiver, a)
-                                except Exception as e:
-                                    client.sendText(receiver, str(e))
-                            elif text.lower() == 'tagall':
-                                group = client.getGroup(msg.to)
-                                nama = [contact.mid for contact in group.members]
-                                nm1, nm2, nm3, nm4, nm5, jml = [], [], [], [], [], len(nama)
-                                if jml <= 100:
-                                    client.mention(msg.to, nama)
-                                if jml > 100 and jml < 200:
-                                    for i in range(0, 100):
-                                        nm1 += [nama[i]]
-                                    client.mention(msg.to, nm1)
-                                    for j in range(101, len(nama)):
-                                        nm2 += [nama[j]]
-                                    client.mention(msg.to, nm2)
-                                if jml > 200 and jml < 300:
-                                    for i in range(0, 100):
-                                        nm1 += [nama[i]]
-                                    client.mention(msg.to, nm1)
-                                    for j in range(101, 200):
-                                        nm2 += [nama[j]]
-                                    client.mention(msg.to, nm2)
-                                    for k in range(201, len(nama)):
-                                        nm3 += [nama[k]]
-                                    client.mention(msg.to, nm3)
-                                if jml > 300 and jml < 400:
-                                    for i in range(0, 100):
-                                        nm1 += [nama[i]]
-                                    client.mention(msg.to, nm1)
-                                    for j in range(101, 200):
-                                        nm2 += [nama[j]]
-                                    client.mention(msg.to, nm2)
-                                    for k in range(201, len(nama)):
-                                        nm3 += [nama[k]]
-                                    client.mention(msg.to, nm3)
-                                    for l in range(301, len(nama)):
-                                        nm4 += [nama[l]]
-                                    client.mention(msg.to, nm4)
-                                if jml > 400 and jml < 501:
-                                    for i in range(0, 100):
-                                        nm1 += [nama[i]]
-                                    client.mention(msg.to, nm1)
-                                    for j in range(101, 200):
-                                        nm2 += [nama[j]]
-                                    client.mention(msg.to, nm2)
-                                    for k in range(201, len(nama)):
-                                        nm3 += [nama[k]]
-                                    client.mention(msg.to, nm3)
-                                    for l in range(301, len(nama)):
-                                        nm4 += [nama[l]]
-                                    client.mention(msg.to, nm4)
-                                    for m in range(401, len(nama)):
-                                        nm5 += [nama[m]]
-                                    client.mention(msg.to, nm5)             
-                                client.sendText(receiver, "Members :"+str(jml))
-                            elif text.lower() == 'scan':
-                                client.sendText(msg.to, "Done!")
+                            may = client.getProfile().mid
+                            if msg.text.lower() == "hai gainbot":
+                                client.sendText(receiver, "Ashiyyapp!")
+                            elif msg.text.lower() == "scan":
+                                client.sendText(receiver, "Done!")
                                 try:
                                     del cctv['point'][msg.to]
                                     del cctv['sidermem'][msg.to]
                                     del cctv['cyduk'][msg.to]
                                 except:
                                     pass
-                                cctv['point'][msg.to] = msg.id
-                                cctv['sidermem'][msg.to] = ""
-                                cctv['cyduk'][msg.to]=True
-                            elif text.lower() == 'sider':
+                                cctv['point'][receiver] = msg.id
+                                cctv['sidermem'][receiver] = ""
+                                cctv['cyduk'][receiver]=True
+                            elif msg.text.lower() == "sider":
                                 if msg.to in cctv['point']:
                                     cctv['cyduk'][msg.to]=False
-                                    client.sendText(msg.to, "Sider Spotted:\n" + cctv['sidermem'][msg.to])
+                                    client.sendText(receiver, "Sider Spotted:\n" + cctv['sidermem'][msg.to])
                                 else:
                                     client.sendText(msg.to, "Please scan your read point first")
-                            elif text.lower() == 'shutdown':
-                                client.sendText(msg.to, "Shutting Down...")
-                                sys.exit()
-                                
-                except Exception as e:
-                    client.log("[SEND_MESSAGE] ERROR : " + str(e))
-
-            elif op.type == OpType.NOTIFIED_READ_MESSAGE:
-                try:
-                    if cctv['cyduk'][op.param1]==True:
-                        if op.param1 in cctv['point']:
-                            Name = client.getContact(op.param2).displayName
-                            if Name in cctv['sidermem'][op.param1]:
-                                pass
                             else:
-                                cctv['sidermem'][op.param1] += Name + "\n"
-                                #pref=['Sider Spotted','Another Sider']
-                                #client.sendText(op.param1, str(random.choice(pref))+' '+Name)
+                                pass
                         else:
                             pass
-                    else:
-                        pass
-                except:
-                    pass
+                    elif op.type == 25:
+                    try:
+                        if msg.contentType == 0:
+                            if msg.toType == 2:
+                                client.sendChatChecked(receiver, msg_id)
+                                contact = client.getContact(sender)
+                                if text.lower() == 'me':
+                                    client.sendMessage(receiver, None, contentMetadata={'mid': sender}, contentType=13)
+                                elif text.lower() == 'speed':
+                                    start = time.time()
+                                    client.sendText(receiver, "TestSpeed")
+                                    elapsed_time = time.time() - start
+                                    client.sendText(receiver, "%s detik" % (elapsed_time))
+                                elif 'spic' in text.lower():
+                                    try:
+                                        key = eval(msg.contentMetadata["MENTION"])
+                                        u = key["MENTIONEES"][0]["M"]
+                                        a = client.getContact(u).pictureStatus
+                                        client.sendImageWithURL(receiver, 'http://dl.profile.line.naver.jp/'+a)
+                                    except Exception as e:
+                                        client.sendText(receiver, str(e))
+                                elif 'scover' in text.lower():
+                                    try:
+                                        key = eval(msg.contentMetadata["MENTION"])
+                                        u = key["MENTIONEES"][0]["M"]
+                                        a = channel.getProfileCoverURL(mid=u)
+                                        client.sendImageWithURL(receiver, a)
+                                    except Exception as e:
+                                        client.sendText(receiver, str(e))
+                                elif text.lower() == 'tagall':
+                                    group = client.getGroup(msg.to)
+                                    nama = [contact.mid for contact in group.members]
+                                    nm1, nm2, nm3, nm4, nm5, jml = [], [], [], [], [], len(nama)
+                                    if jml <= 100:
+                                        client.mention(msg.to, nama)
+                                    if jml > 100 and jml < 200:
+                                        for i in range(0, 100):
+                                            nm1 += [nama[i]]
+                                        client.mention(msg.to, nm1)
+                                        for j in range(101, len(nama)):
+                                            nm2 += [nama[j]]
+                                        client.mention(msg.to, nm2)
+                                    if jml > 200 and jml < 300:
+                                        for i in range(0, 100):
+                                            nm1 += [nama[i]]
+                                        client.mention(msg.to, nm1)
+                                        for j in range(101, 200):
+                                            nm2 += [nama[j]]
+                                        client.mention(msg.to, nm2)
+                                        for k in range(201, len(nama)):
+                                            nm3 += [nama[k]]
+                                        client.mention(msg.to, nm3)
+                                    if jml > 300 and jml < 400:
+                                        for i in range(0, 100):
+                                            nm1 += [nama[i]]
+                                        client.mention(msg.to, nm1)
+                                        for j in range(101, 200):
+                                            nm2 += [nama[j]]
+                                        client.mention(msg.to, nm2)
+                                        for k in range(201, len(nama)):
+                                            nm3 += [nama[k]]
+                                        client.mention(msg.to, nm3)
+                                        for l in range(301, len(nama)):
+                                            nm4 += [nama[l]]
+                                        client.mention(msg.to, nm4)
+                                    if jml > 400 and jml < 501:
+                                        for i in range(0, 100):
+                                            nm1 += [nama[i]]
+                                        client.mention(msg.to, nm1)
+                                        for j in range(101, 200):
+                                            nm2 += [nama[j]]
+                                        client.mention(msg.to, nm2)
+                                        for k in range(201, len(nama)):
+                                            nm3 += [nama[k]]
+                                        client.mention(msg.to, nm3)
+                                        for l in range(301, len(nama)):
+                                            nm4 += [nama[l]]
+                                        client.mention(msg.to, nm4)
+                                        for m in range(401, len(nama)):
+                                            nm5 += [nama[m]]
+                                        client.mention(msg.to, nm5)             
+                                    client.sendText(receiver, "Members :"+str(jml))
+                                elif text.lower() == 'scan':
+                                    client.sendText(msg.to, "Done!")
+                                    try:
+                                        del cctv['point'][msg.to]
+                                        del cctv['sidermem'][msg.to]
+                                        del cctv['cyduk'][msg.to]
+                                    except:
+                                        pass
+                                    cctv['point'][msg.to] = msg.id
+                                    cctv['sidermem'][msg.to] = ""
+                                    cctv['cyduk'][msg.to]=True
+                                elif text.lower() == 'sider':
+                                    if msg.to in cctv['point']:
+                                        cctv['cyduk'][msg.to]=False
+                                        client.sendText(msg.to, "Sider Spotted:\n" + cctv['sidermem'][msg.to])
+                                    else:
+                                        client.sendText(msg.to, "Please scan your read point first")
+                                elif text.lower() == 'shutdown':
+                                    client.sendText(msg.to, "Shutting Down...")
+                                    sys.exit()
+                                
+                    except Exception as e:
+                        client.log("[SEND_MESSAGE] ERROR : " + str(e))
 
-            else:
-                pass
+                elif op.type == OpType.NOTIFIED_READ_MESSAGE:
+                    try:
+                        if cctv['cyduk'][op.param1]==True:
+                            if op.param1 in cctv['point']:
+                                Name = client.getContact(op.param2).displayName
+                                if Name in cctv['sidermem'][op.param1]:
+                                    pass
+                                else:
+                                    cctv['sidermem'][op.param1] += Name + "\n"
+                                    #pref=['Sider Spotted','Another Sider']
+                                    #client.sendText(op.param1, str(random.choice(pref))+' '+Name)
+                            else:
+                                pass
+                        else:
+                            pass
+                    except:
+                        pass
+
+                else:
+                    pass
 
             # Don't remove this line, if you wan't get error soon!
             poll.setRevision(op.revision)
